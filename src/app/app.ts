639d753373aac15bd439745796db1fbbb9228544
Angelic Ann La Paz <angelicannlapaz24@gmail.com>
2026-05-20 09:15:39 -0400

    correcting navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,33 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { NgClass, NgIf } from '@angular/common';
+import { NgIf } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 import { AccountService } from './_services';
 import { Account, Role } from './_models';
 import { AlertComponent } from './_components';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.html',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    RouterLinkActive,
-    NgClass,
-    NgIf,
-    AlertComponent
-  ]
+    selector: 'app-root',
+    standalone: true,
+    imports: [NgIf, RouterLink, RouterLinkActive, RouterOutlet, AlertComponent],
+    templateUrl: 'app.html'  // or whatever your template file is named
 })
 export class App {
-  Role = Role;
-  account?: Account | null;
+    Role = Role;
+    account?: Account | null;
 
-  constructor(private accountService: AccountService) {
-    this.accountService.account.subscribe(x => this.account = x);
-  }
+    constructor(private accountService: AccountService) {
+        this.accountService.account.subscribe(x => this.account = x);
+    }
 
-  logout() {
-    this.accountService.logout();
-  }
+    logout() {
+        this.accountService.logout();
+    }
 }
